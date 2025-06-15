ed5469cbbd711e970cd7b94f65b726d9b889596f
sp-mixer <daiske.111858@gmail.com>
2025-06-15 18:53:03 +0900

    ban修正

--- web6/Filters/Auth1Attribute.cs
+++ web6/Filters/Auth1Attribute.cs
@@ -6,9 +6,13 @@ public class Auth1Attribute : ActionFilterAttribute {
     public override void OnActionExecuting(ActionExecutingContext context) {
         var session = context.HttpContext.Session;
         var auth1 = session.GetString("Auth1");
+        var ban = session.GetString("Ban");
 
         if (auth1 != "True" && auth1 != "1") {
             // 権限なし → ホーム画面へ
+            context.Result = new RedirectResult("/Home/Index");
+        }
+        if ((auth1 != "True" && auth1 != "1") || (ban == "True" || ban == "1")) {
             context.Result = new RedirectResult("/Home/Index");
         }
     }
